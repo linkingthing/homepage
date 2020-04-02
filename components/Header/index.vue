670c9e53be72ab006f3dc6f31a18483c77a81c2b
dontknowly <314589318@qq.com>
2020-04-02 18:04:52 +0800

    修改header

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -18,10 +18,11 @@
         <el-menu-item index="2-2"><nuxt-link to="/monitor-platform">IPV6发展监测平台</nuxt-link></el-menu-item>
         <el-menu-item index="2-3"><nuxt-link to="/promotion-manage">IPV6代际升迁管理</nuxt-link></el-menu-item>
       </el-submenu>
-      <el-menu-item index="3">合作代理</el-menu-item>
-      <el-menu-item index="4">新闻中心</el-menu-item>
-      <el-menu-item index="5">下载中心</el-menu-item>
-      <el-menu-item index="6">关于我们</el-menu-item>
+      <el-menu-item index="3"><nuxt-link to="/cooperate">合作代理</nuxt-link></el-menu-item>
+      <el-menu-item index="4"><nuxt-link to="/news">新闻中心</nuxt-link></el-menu-item>
+      <el-menu-item index="5"><nuxt-link to="/support">支持中心</nuxt-link></el-menu-item>
+      <el-menu-item index="6"><nuxt-link to="/download">下载中心</nuxt-link></el-menu-item>
+      <el-menu-item index="7"><nuxt-link to="/about">关于我们</nuxt-link></el-menu-item>
     </el-menu>
   </div>
 </template>
@@ -33,8 +34,63 @@ export default {
   data(){
     return {
       activeIndex:"1",
-      src
+      src,
+      routes:[
+        {
+          path:"/home",
+          label:"首页",
+          index:"1"
+        },
+        {
+          path:"/clxone-ddi",
+          label:"CLXOne™DDI",
+          index:"2-1"
+        },
+        {
+          path:"/monitor-platform",
+          label:"IPV6发展监测平台",
+          index:"2-2"
+        },
+        {
+          path:"/promotion-manage",
+          label:"IPV6代际升迁管理",
+          index:"2-3"
+        },
+        {
+          path:"/cooperate",
+          label:"合作代理",
+          index:"3"
+        },
+        {
+          path:"/news",
+          label:"新闻中心",
+          index:"4"
+        },
+        {
+          path:"/support",
+          label:"支持中心",
+          index:"5"
+        },
+        {
+          path:"/download",
+          label:"下载中心",
+          index:"6"
+        },
+        {
+          path:"/about",
+          label:"关于我们",
+          index:"7"
+        }
+      ]
     };
+  },
+
+  created(){
+    const path = this.routes.find(item => item.path === this.$router.currentRoute.path);
+
+    if(path){
+      this.activeIndex = path.index;
+    }    
   },
 
   methods:{
